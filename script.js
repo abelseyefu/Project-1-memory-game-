9efff272653abdf5f9ab61bbbe7a0c84e62a4f3d
abelseyefu <seyefuabel@gmail.com>
2022-04-30 14:54:46 -0400

    assigned cards their values

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const timer = document.getElementById("timer")
 let card1 = document.querySelector(`[data-card = "diamonds-A"]`)
 // let card1 = document.getElementById("diamonds-A")
-let card2 = document.getElementById(`[data-card = "diamonds-A"]`)
-let card3 = document.getElementById("hearts-A")
-let card4 = document.getElementById("joker-red")
-let card5 = document.getElementById("spades-A")
-let card6 = document.getElementById("hearts-A1")
-let card7 = document.getElementById("clubs-A1")
-let card8 = document.getElementById("diamonds-A1")
-let card9 = document.getElementById("spades-A1")
-let card10 = document.getElementById("joker-red1")
+let card2 = document.querySelector(`[data-card = "clubs-A"]`)
+let card3 = document.querySelector(`[data-card = "hearts-A"]`)
+let card4 = document.querySelector( `[data-card = "joker-red"]`)
+let card5 = document.querySelector( `[data-card = "spades-A"]`)
+let card6 = document.querySelectorAll(`[data-card = "hearts-A"]`)[1]
+let card7 = document.querySelectorAll(`[data-card = "clubs-A"]`)[1]
+let card8 = document.querySelectorAll(`[data-card = "diamonds-A"]`)[1]
+let card9 = document.querySelectorAll(`[data-card = "spades-A"]`)[1]
+let card10 = document.querySelectorAll(`[data-card = "joker-red"]`)[1]
 
 let cardArry = [card1, card2, card3, card4, card5, card5, card6, card7, card8, card9, card10]
 

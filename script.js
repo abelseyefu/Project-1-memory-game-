fee9233894f04e600ee774bba884c0af37e6bc5e
abelseyefu <seyefuabel@gmail.com>
2022-05-04 20:59:02 -0400

    added logic for reset and score count

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const timer = document.getElementById("timer")
 let card1 = document.querySelector(`[data-card = "diamonds-A"]`)
-// let card1 = document.getElementById("diamonds-A")
 let card2 = document.querySelector(`[data-card = "clubs-A"]`)
 let card3 = document.querySelector(`[data-card = "hearts-A"]`)
-let card4 = document.querySelector( `[data-card = "joker-red"]`)
-let card5 = document.querySelector( `[data-card = "spades-A"]`)
+let card4 = document.querySelector(`[data-card = "joker-red"]`)
+let card5 = document.querySelector(`[data-card = "spades-A"]`)
 let card6 = document.querySelectorAll(`[data-card = "hearts-A"]`)[1]
 let card7 = document.querySelectorAll(`[data-card = "clubs-A"]`)[1]
 let card8 = document.querySelectorAll(`[data-card = "diamonds-A"]`)[1]
@@ -15,26 +14,23 @@ let cardArry = [card1, card2, card3, card4, card5, card5, card6, card7, card8, c
 
 
 
-// // function gameFunction(params) {
-//     let matching = document.querySelectorAll(".cards").addEventListener("click", function show(e) {
-//         return 
-//     })
-
 let clicks = 0
 let selectedCards = []
+turns = 0
 cardArry.forEach((card) => {
     card.addEventListener("click", function (e) {
         console.log(e.target.id)
         card.setAttribute("src", "")
         card.setAttribute("class", e.target.id)
         clicks += 1
-        // if #of clicks =1 
+
+
         if (clicks == 1) {
-            // start timer
+
             console.log(selectedCards)
 
             selectedCards.push(e.target)
-            
+
 
         } else if (clicks == 2) {
             console.log(selectedCards)
@@ -42,14 +38,18 @@ cardArry.forEach((card) => {
                 selectedCards.push(e.target)
                 flipCards()
             }, 100)
+
+
+            document.getElementById("numOfTurns").innerHTML = turns += 1;
+
             function flipCards() {
                 if (selectedCards[0].dataset.card != selectedCards[1].dataset.card) {
                     console.log(selectedCards)
                     selectedCards[0].classList.add("back-red")
                     selectedCards[1].classList.add('back-red')
                 }
-                    clicks = 0
-                    selectedCards = []
+                clicks = 0
+                selectedCards = []
             }
         }
         // start timer
@@ -62,6 +62,5 @@ cardArry.forEach((card) => {
         // reset selectedCard = 0
     })
 })
-
 
 
